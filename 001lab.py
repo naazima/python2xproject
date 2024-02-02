26a2be5d1c380e91d52c75d76b9ad21eca273981
NaazAfreen <naazafreen@NaazAfreens-MBP.attlocal.net>
2024-02-01 22:26:20 -0500

    Initial commit

---
+++ 001lab.py
@@ -0,0 +1,17 @@
+print("Hi Afreen Naaz")
+print("hello world", "checking the keyword", end="\n", sep="$")
+import keyword
+
+print(keyword.kwlist)
+age = 45
+char = 'name'
+print(type(age))
+print(type(char))
+
+name = input("what is your name")
+age = input("what is your age")
+roll_no = input("what's your roll no")
+phone_no = input("what is your phone no")
+print("name", "age", "roll_no", 'phone_no', sep="")
+
+
