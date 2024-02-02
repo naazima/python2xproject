aa4d385d7ec4365812e35cb3fd52eb4617d6f7ef
naazima <45152631+naazima@users.noreply.github.com>
2024-02-01 22:34:59 -0500

    Update 001lab.py
    Added comment

--- 001lab.py
+++ 001lab.py
@@ -8,10 +8,16 @@ char = 'name'
 print(type(age))
 print(type(char))
 
+# Home work : Week 1- TASK 1:
+-------------------------------
+
 name = input("what is your name")
 age = input("what is your age")
 roll_no = input("what's your roll no")
 phone_no = input("what is your phone no")
 print("name", "age", "roll_no", 'phone_no', sep="")
+
+# question : How can I use sep keyword to incorporate space inbetween 
+# my Input function value  and Print value?
 
 
